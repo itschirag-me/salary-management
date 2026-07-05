0df2d1182efb3165e5ff618267994c6ba5e273af
itschirag-me <imchirag2000@gmail.com>
2026-07-05 16:53:51 +0530

    feat: initialize NestJS server project with health check endpoint, configuration validation, and Docker support

---
+++ server/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { validationSchema } from 'config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: validationSchema,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ server/src/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getHealth() {
+    return {
+      status: 'OK',
+      message: 'OK',
+      data: {
+        service: 'Salary Management System',
+        version: 'v1',
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+      }
+    }
+  }
+}
